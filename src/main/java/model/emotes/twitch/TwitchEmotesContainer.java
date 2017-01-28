b3e579b0be19a5b90c7ee95fbb80e79fc49187e5
Vladimir <crimsonandred@mail.ru>
2017-01-28 12:33:59 +0300

    Singleton adjustment

--- src/main/java/model/emotes/twitch/TwitchEmotesContainer.java
+++ src/main/java/model/emotes/twitch/TwitchEmotesContainer.java
@@ -15,16 +15,23 @@ public class TwitchEmotesContainer implements EmotesContainer {
 	private TwitchEmotesContainer(int capacity) {
 		// Should it be concurrent??
 		//TODO decide later
-		//	map = new ConcurrentHashMap<>(capacity);
+		//JavaDoc says that:
+		//If multiple threads access a hash map concurrently, and at least one of the threads modifies the map structurally, it must be synchronized externally.
 		map = new HashMap<>(capacity);
+		//	map = new ConcurrentHashMap<>(capacity);
 	}
 
 	public static TwitchEmotesContainer getInstance() {
 		return singleton;
 	}
 
-	public static TwitchEmotesContainer setInstance(int capacity) {
-		singleton = new TwitchEmotesContainer(capacity);
+
+	// Dont need double check lock here
+	// setInstance is invoked only once
+	public static synchronized TwitchEmotesContainer setInstance(int capacity) {
+		if (singleton == null) {
+			singleton = new TwitchEmotesContainer(capacity);
+		}
 		return singleton;
 	}
 
